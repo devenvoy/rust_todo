c0b599221208cfefe43912ac29772e2498b876ff
devenvoy <iambeast0069@gmail.com>
2025-04-25 17:44:16 +0530

    GUI App Statrted

--- src/todo_model/mod.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Todo {
-    pub id: Option<u32>,
-    pub content: String,
-    pub done: bool,
-}
